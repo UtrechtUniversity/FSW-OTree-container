1214df07d5df07981ae269c288e9334a5c7a3248
Sandor Spruit <A.G.L.Spruit@uu.nl>
2024-03-26 10:42:59 +0100

    Skip an initialisation step

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 # If run for the first time, set up databases
-if [ ! -f "/opt/init/.done" ]; then
-    /usr/bin/env python -u /usr/local/bin/otree resetdb --noinput \
-    && touch /opt/init/.done
-fi
+#if [ ! -f "/opt/init/.done" ]; then
+#    /usr/bin/env python -u /usr/local/bin/otree resetdb --noinput \
+#    && touch /opt/init/.done
+#fi
 # Start oTree server
 cd /opt/otree && otree prodserver 8080
