3e4d87c90bd1ed6ad5cdbd1c725a138858f0e8fd
Sprui101 <A.G.L.Spruit@uu.nl>
2024-07-23 15:14:46 +0200

    Tweaking entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 # If run for the first time, set up databases
 if [ ! -f "/home/student/init/.done" ]; then
     git clone https://${GITHUB_USER}:${ACCESS_TOKEN}@github.com/UtrechtUniversity/${REPOSITORY}
+    cd /home/student/${REPOSITORY}
+    pip install -f requirements.txt
     python -u /usr/local/bin/otree resetdb --noinput && touch /home/student/init/.done
 fi
 # Start oTree server
 cd /home/student/${REPOSITORY} \
-  && pip install -f requirements.txt \
   && export PATH=$PATH:~/.local/bin \
   && otree prodserver
