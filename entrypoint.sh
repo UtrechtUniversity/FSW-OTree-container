ed4f4805cbf1cc76965f6c4e1e75dabe79726b3d
Sprui101 <A.G.L.Spruit@uu.nl>
2025-04-03 14:56:21 +0200

    Tweaking entrypoint, additional checks, especially on the Git clone command that can fail with weird consequences

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,20 @@
 # If run for the first time, set up databases
 if [ ! -f "/home/student/init/.done" ]; then
     git clone https://${GITHUB_USER}:${ACCESS_TOKEN}@github.com/UtrechtUniversity/${REPOSITORY}
-    cd /home/student/${REPOSITORY}
+    returnValue=$?
+    if ! test "returnValue" -eq 0
+    then
+        echo "Git clone failed, you may want to check your access token"
+        exit 1
+    fi
+    cd /home/student/${REPOSITORY} || { echo "Cannot cd into repository"; exit 1; }
     pip install -r requirements.txt
+    returnValue=$?
+    if ! test "returnValue" -eq 0
+    then
+        echo "Could not install requirements"
+        exit 1
+    fi
     python -u /usr/local/bin/otree resetdb --noinput && touch /home/student/init/.done
 fi
 # Start oTree server
