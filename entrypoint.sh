ea532578416e73254e30730f5f51d8547e923f27
Sprui101 <A.G.L.Spruit@uu.nl>
2025-05-09 12:16:31 +0200

    Adding the option to pull a specific branch of an experiment from Github, so we can try different variants as we tweak parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
+# Use parameter expansion to split a string '<repository>@<branch>'
+repo=${REPOSITORY%%@*}
+branch=${REPOSITORY#*@}
+
 # If run for the first time, set up databases
 if [ ! -f "/home/student/init/.done" ]; then
-    # Use parameter expansion to split a string '<repository>@<branch>'
-    repo=${REPOSITORY%%@*}
-    branch=${REPOSITORY#*@}
     # when the '@' character is missing, both regex patterns will match
     if [ "$branch" == "$repo" ]; then
       echo "repo without branch"
-      git clone https://${GITHUB_USER}:${ACCESS_TOKEN}@github.com/UtrechtUniversity/${REPOSITORY}
+      git clone https://${GITHUB_USER}:${ACCESS_TOKEN}@github.com/UtrechtUniversity/${repo}
     else
       echo "have a branch '$branch'"
       git clone -b ${branch} --single-branch https://${GITHUB_USER}:${ACCESS_TOKEN}@github.com/UtrechtUniversity/${repo}
@@ -18,7 +19,7 @@ if [ ! -f "/home/student/init/.done" ]; then
       exit 1
     fi
 
-    cd /home/student/${REPOSITORY} || { echo "Cannot cd into repository"; exit 1; }
+    cd /home/student/${repo} || { echo "Cannot cd into repository"; exit 1; }
     pip install -r requirements.txt
     if [ $? -eq 0 ]; then
         echo "OK"
@@ -29,6 +30,6 @@ if [ ! -f "/home/student/init/.done" ]; then
     python -u /home/student/.local/bin/otree resetdb --noinput && touch /home/student/init/.done
 fi
 # Start oTree server
-cd /home/student/${REPOSITORY} \
+cd /home/student/${repo} \
   && export PATH=$PATH:~/.local/bin \
   && otree prodserver
